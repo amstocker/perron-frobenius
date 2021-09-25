79dfe665c34070817397b125ab9c58f0e55d7386
Andrew Stocker <sleepyams@gmail.com>
2021-09-25 15:27:04 -0600

    Update lim.py

--- lim.py
+++ lim.py
@@ -4,16 +4,15 @@ from numpy import linalg as la
 N = 100
 guess = np.sqrt(2)
 
-A0 = np.array([[0,1], [1,0]])
+a = np.array([[0,1], [1,0]])
 
-A = A0
-lastA = A
+prev = a
 for i in range(1, N):
-    A = np.zeros((2*i + 2, 2*i + 2))
-    A[0:2*i, 0:2*i] = lastA
-    A[2*i, 2*i - 2] = 1
-    A[2*i - 1, 2*i + 1] = 1
-    A[2*i + 1, 2*i] = 1
-    lastA = A
-    l = np.amax(np.absolute(la.eigvals(A)))
-    print("{}:".format(i), l, "(error: {})".format(guess - l))
+    a = np.zeros((2*i + 2, 2*i + 2))
+    a[0:2*i, 0:2*i] = last
+    a[2*i, 2*i - 2] = 1
+    a[2*i - 1, 2*i + 1] = 1
+    a[2*i + 1, 2*i] = 1
+    last = a
+    r = np.amax(np.absolute(la.eigvals(a)))
+    print("{}:".format(i), r, "(error: {})".format(guess - r))
