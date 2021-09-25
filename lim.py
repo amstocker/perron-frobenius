b2aee72f046b4dab88ef0d3d3581cab60d119b97
Andrew Stocker <sleepyams@gmail.com>
2021-09-25 15:33:16 -0600

    Update lim.py

--- lim.py
+++ lim.py
@@ -1,18 +1,23 @@
 import numpy as np
 from numpy import linalg as la
 
-N = 100
+N = 50
+
+# We are guessing that the limit of the Perron-Frobenius
+#   eigenvalues is sqrt(2).
 guess = np.sqrt(2)
 
 a = np.array([[0,1], [1,0]])
 
-prev = a
 for i in range(1, N):
+    prev = a
+    
     a = np.zeros((2*i + 2, 2*i + 2))
-    a[0:2*i, 0:2*i] = last
+    a[0:2*i, 0:2*i] = prev
     a[2*i, 2*i - 2] = 1
     a[2*i - 1, 2*i + 1] = 1
     a[2*i + 1, 2*i] = 1
-    last = a
+    
+    # spectral radius
     r = np.amax(np.absolute(la.eigvals(a)))
     print("{}:".format(i), r, "(error: {})".format(guess - r))
